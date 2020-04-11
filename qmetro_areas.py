11437ca5658bb4969539f747e7fe2a22d7bd962a
David DeBoer <ddeboer@berkeley.edu>
2020-04-11 09:50:04 -0700

    add Bin_State

--- qmetro_areas.py
+++ qmetro_areas.py
@@ -1,9 +1,10 @@
 import json
-from . import get_fip, mymaps_utils
 
 
 def get(area_type):
-    metro_area_file = mymaps_utils.datafn('metro_areas.json')
+    with open('statefp.json', 'r') as fp:
+        sfpjson = json.load(fp)
+    metro_area_file = 'metro_areas.json'
     with open(metro_area_file, 'r') as fp:
         metro = json.load(fp)
     arty = {'districts': ['Congress_District', 'cd116fp'],
@@ -17,11 +18,7 @@ def get(area_type):
     pma = {}
     for abbr, data in metro.items():
         pma[abbr] = []
-        is_state = get_fip.state(data['state'])
-        if not is_state:
-            print("Not finding {}: {}".format(abbr, data['state']))
-            continue
-        statefp = is_state[0]
+        statefp = sfpjson[data['state']][0]
         for distr in data[area_type]:
             if area_type == "districts":
                 pma[abbr].append('{}-{:02d}'.format(statefp, distr))
